88508f9bfe41a02b7eafa72fa4e5a1ea90010b33
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-04-15 09:53:08 +0200

    Smoothed animations

--- projects/ng-animations/src/lib/animations/bubble-in.animation.ts
+++ projects/ng-animations/src/lib/animations/bubble-in.animation.ts
@@ -10,7 +10,7 @@ export function bubble(animationConfig: AnimationConfig = {}, key: string = 'bub
         `${animationConfig.inTime}ms ${animationConfig.inDelay}ms ${animationConfig.easing}`,
         keyframes([
           style({ transform: 'scale(0)', offset: 0 }),
-          style({ transform: 'scale(1.5)', offset: .5 }),
+          style({ transform: 'scale(1.5)', offset: .3 }),
           style({ transform: 'scale(1)', offset: 1 })
         ])
       )
