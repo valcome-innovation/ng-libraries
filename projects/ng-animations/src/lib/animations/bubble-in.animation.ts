388edf35cabb5ac836ea285cd7b66af4e7d7efcc
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-04-15 09:58:02 +0200

    More smoothen the animation

--- projects/ng-animations/src/lib/animations/bubble-in.animation.ts
+++ projects/ng-animations/src/lib/animations/bubble-in.animation.ts
@@ -9,8 +9,9 @@ export function bubble(animationConfig: AnimationConfig = {}, key: string = 'bub
       animate(
         `${animationConfig.inTime}ms ${animationConfig.inDelay}ms ${animationConfig.easing}`,
         keyframes([
-          style({ transform: 'scale(0)', offset: 0 }),
-          style({ transform: 'scale(1.5)', offset: .3 }),
+          style({ transform: 'scale(.5)', opacity: 0, offset: 0 }),
+          style({ transform: 'scale(1)', opacity: 1, offset: .2 }),
+          style({ transform: 'scale(1.5)', offset: .4 }),
           style({ transform: 'scale(1)', offset: 1 })
         ])
       )
