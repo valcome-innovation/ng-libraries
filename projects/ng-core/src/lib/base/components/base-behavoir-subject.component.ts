04d3dc15d06421f437a3ae7f8bbf2ea3454558d0
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-05-08 12:09:52 +0200

    Made base Behavior subject immutable

--- projects/ng-core/src/lib/base/components/base-behavoir-subject.component.ts
+++ projects/ng-core/src/lib/base/components/base-behavoir-subject.component.ts
@@ -1,9 +1,11 @@
 import { BehaviorSubject } from 'rxjs';
 import { BaseSubscriptionComponent } from './base-subscription.component';
+import { JavascriptUtils } from '../../utils/javascript.utils';
 
 export class BaseBehaviorSubjectComponent extends BaseSubscriptionComponent {
-  protected listen(behaviorSubject: BehaviorSubject<any>, onChange: (value: any) => void): void {
-    onChange(behaviorSubject?.getValue());
+  protected listen<T>(behaviorSubject: BehaviorSubject<T>, onChange: (value: T) => void): void {
+    let newInstance: any = JavascriptUtils.clone<T>(behaviorSubject?.getValue());
+    onChange(newInstance);
     this.addSub(behaviorSubject?.subscribe(onChange));
   }
 }
