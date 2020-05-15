4d9dca12b15801b21867969589b437a1ccc44d92
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-05-15 14:05:18 +0200

    Added InitilazableService

---
+++ projects/ng-core/src/lib/base/services/base-initializable.service.spec.ts
@@ -0,0 +1,54 @@
+import { BaseInitializableService } from './base-initializable.service';
+
+describe('BaseInitializableService', () => {
+
+  let service: BaseInitializableServiceSpec;
+
+  beforeEach(() => {
+    service = new BaseInitializableServiceSpec();
+  })
+
+  it('should create', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should not be initialzed on creation', () => {
+    expect(service.isInitialized()).toBeFalsy();
+  });
+
+  it('should initialize after calling', () => {
+    service.initialize();
+    expect(service.isInitialized()).toBeTruthy();
+  });
+
+  it('should wait for initialization', async () => {
+    setTimeout(() => service.initialize(), 250);
+
+    await service.waitUntilInitialized();
+    expect(service.isInitialized()).toBeTruthy();
+  });
+
+  it('should pass if preInitialised', async () => {
+    service.initialize();
+    await service.waitUntilInitialized();
+    expect(service.isInitialized()).toBeTruthy();
+  });
+
+  it('should catch errors', done => {
+    service.createError();
+    service.waitUntilInitialized().catch((e) => {
+      expect(e).toBeDefined();
+      done()
+    });
+  });
+})
+
+class BaseInitializableServiceSpec extends BaseInitializableService {
+  public initialize(): void {
+    this.isInitialized$.next(true);
+  }
+
+  public createError(): void {
+    this.isInitialized$ = null;
+  }
+}
---
+++ projects/ng-core/src/lib/base/services/base-initializable.service.ts
@@ -0,0 +1,28 @@
+import { BehaviorSubject } from 'rxjs';
+
+export abstract class BaseInitializableService {
+  
+  protected isInitialized$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  public isInitialized(): boolean {
+    return this.isInitialized$.getValue();
+  }
+
+  public waitUntilInitialized(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      try {
+        if (this.isInitialized()) {
+          resolve();
+        } else {
+          this.isInitialized$.subscribe((isInitialized: boolean) => {
+            if (isInitialized) {
+              resolve();
+            }
+          })
+        }
+      } catch (e) {
+        reject(e);
+      }
+    })
+  }
+}
