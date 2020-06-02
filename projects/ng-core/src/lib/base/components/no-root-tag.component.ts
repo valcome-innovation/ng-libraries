54f47c023dfefc6edc079a17763e8368e6cbe756
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-06-02 14:54:55 +0200

    Adde no root tag

---
+++ projects/ng-core/src/lib/base/components/no-root-tag.component.ts
@@ -0,0 +1,16 @@
+import { OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { BaseComponent } from 'ng-core';
+
+export class NoRootTagComponent extends BaseComponent implements OnInit {
+
+  @ViewChild(TemplateRef, { static: true })
+  public template: TemplateRef<void>;
+
+  public constructor(private vcRef: ViewContainerRef) {
+    super();
+  }
+
+  public ngOnInit(): void {
+    this.vcRef.createEmbeddedView(this.template);
+  }
+}
