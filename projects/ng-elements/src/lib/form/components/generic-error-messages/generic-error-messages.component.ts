bbf953817f420098057838e22dea882739bf3fe8
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-06-02 14:16:45 +0200

    ngHost

--- projects/ng-elements/src/lib/form/components/generic-error-messages/generic-error-messages.component.ts
+++ projects/ng-elements/src/lib/form/components/generic-error-messages/generic-error-messages.component.ts
@@ -3,6 +3,7 @@ import { FormErrorType } from '../../model/form-error-type';
 
 @Component({
   selector: 'val-generic-error-messages',
+  host: { ngNoHost: '' },
   templateUrl: './generic-error-messages.component.html'
 })
 export class GenericErrorMessagesComponent {
