cb6212582fc56a88d2532cbfc27afa3aab9323fa
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-06-02 15:13:04 +0200

    using template component

--- projects/ng-elements/src/lib/form/components/generic-error-messages/generic-error-messages.component.ts
+++ projects/ng-elements/src/lib/form/components/generic-error-messages/generic-error-messages.component.ts
@@ -2,12 +2,9 @@ import { Component } from '@angular/core';
 import { FormErrorType } from '../../model/form-error-type';
 
 @Component({
-  selector: 'val-generic-error-messages',
+  selector: '[val-generic-error-messages]',
   templateUrl: './generic-error-messages.component.html'
 })
 export class GenericErrorMessagesComponent {
   public FormErrorType = FormErrorType;
-
-  public constructor() {
-  }
 }
