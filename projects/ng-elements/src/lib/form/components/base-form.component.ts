7f6f59f0b045fc78459b02d4d28fc4d13598bba2
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2020-06-05 14:50:42 +0200

    Added input auto fill enum

--- projects/ng-elements/src/lib/form/components/base-form.component.ts
+++ projects/ng-elements/src/lib/form/components/base-form.component.ts
@@ -1,11 +1,16 @@
 import { BaseComponent } from '@valcome/ng-core';
 import { FormGroup } from '@angular/forms';
+import { FormErrorType } from '../model/form-error-type';
+import { InputAutofill } from '../model/input-autofill';
 
 export class BaseFormComponent extends BaseComponent {
 
   public form: FormGroup;
   public isLoading: boolean;
   public isSubmitted: boolean = false;
+
+  public FormErrorType = FormErrorType;
+  public InputAutofill = InputAutofill;
 
   public resetSubmission(): void {
     this.isSubmitted = false;
