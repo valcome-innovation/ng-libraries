f3b3131aa6ab3e0e01aa8982894faf125b2dc551
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-07-01 15:25:56 +0200

    Added toasts 🍞

---
+++ projects/ng-elements/src/lib/toaster/shared/dom.service.ts
@@ -0,0 +1,53 @@
+import { INotyfStyle } from './inotyf-style';
+import {
+  ApplicationRef,
+  ComponentFactoryResolver,
+  ComponentRef,
+  EmbeddedViewRef,
+  Injectable,
+  Injector
+} from '@angular/core';
+
+@Injectable()
+export class DomService {
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
+              private appRef: ApplicationRef,
+              private injector: Injector) {
+  }
+
+  createComponentRef(component: any): ComponentRef<any> {
+    const componentRef = this.componentFactoryResolver
+      .resolveComponentFactory(component)
+      .create(this.injector);
+    this.appRef.attachView(componentRef.hostView);
+    return componentRef;
+  }
+
+  getDomElementFromComponentRef(componentRef: ComponentRef<any>): HTMLElement {
+    return (componentRef.hostView as EmbeddedViewRef<any>)
+      .rootNodes[0] as HTMLElement;
+  }
+
+  addChild(child: HTMLElement, parent: HTMLElement = document.body) {
+    parent.appendChild(child);
+  }
+
+  destroyRef(componentRef: ComponentRef<any>, delay: number) {
+    setTimeout(() => {
+      this.appRef.detachView(componentRef.hostView);
+      componentRef.destroy();
+    }, delay);
+  }
+
+  setDynamicStyles(styles: INotyfStyle, componentRef: ComponentRef<any>) {
+    Object.keys(styles).forEach(cssRule => {
+      let cssValue = styles[cssRule];
+      componentRef.instance.renderer.setElementStyle(
+        componentRef.instance.elementRef.nativeElement,
+        cssRule,
+        cssValue
+      );
+    });
+  }
+}
---
+++ projects/ng-elements/src/lib/toaster/toaster.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastComponent } from './toast/toast.component';
+import { ToastContainerComponent } from './toast-container/toast-container.component';
+import { NotyfService } from './shared/notyf.service';
+import { DomService } from './shared/dom.service';
+
+export { DomService } from './shared/dom.service';
+export { NotyfService } from './shared/notyf.service';
+export { ToastComponent } from './toast/toast.component';
+export { ToastContainerComponent } from './toast-container/toast-container.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [ToastComponent, ToastContainerComponent],
+  exports: [],
+  entryComponents: [ToastComponent, ToastContainerComponent],
+  providers: [NotyfService, DomService]
+})
+export class ToasterModule {
+}
