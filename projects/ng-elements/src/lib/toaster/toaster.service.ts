353aeade3a8075482b41aa039689aae59600e082
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-07-01 17:33:50 +0200

    Fixes

--- projects/ng-elements/src/lib/toaster/toaster.service.ts
+++ projects/ng-elements/src/lib/toaster/toaster.service.ts
@@ -1,14 +1,13 @@
 import { Inject, Injectable } from '@angular/core';
-import { ModalModule } from '../modals/modal.module';
-import { INotyfOptions, Notyf } from 'notyf';
-import { TOASTER_OPTIONS } from './toaster.module';
+import { Notyf } from 'notyf';
+import { TOASTER_OPTIONS, ToasterModule } from './toaster.module';
 import 'notyf/notyf.min.css';
 
-@Injectable({ providedIn: ModalModule })
+@Injectable({ providedIn: ToasterModule })
 export class ToasterService {
   public readonly toaster: Notyf;
 
-  public constructor(@Inject(TOASTER_OPTIONS) options: Partial<INotyfOptions>) {
+  public constructor(@Inject(TOASTER_OPTIONS) options) {
     this.toaster = new Notyf(options);
   }
 }
