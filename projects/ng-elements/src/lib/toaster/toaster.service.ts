dffe5317d41ee5af3151ca2258c109c6fe852754
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-07-01 18:09:09 +0200

    Removed css import

--- projects/ng-elements/src/lib/toaster/toaster.service.ts
+++ projects/ng-elements/src/lib/toaster/toaster.service.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { Notyf } from 'notyf';
 import { TOASTER_OPTIONS, ToasterModule } from './toaster.module';
-import 'notyf/notyf.min.css';
 
 @Injectable({ providedIn: ToasterModule })
 export class ToasterService {
