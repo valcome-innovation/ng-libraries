a98683414f0e64602cf1aabcf44ab48d5beb2c34
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2020-07-02 09:35:04 +0200

    Add generic password field

---
+++ projects/ng-elements/src/lib/form/components/generic-password/generic-password.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { BaseGenericFieldComponent } from '../base-generic-field.component';
+
+@Component({
+  selector: 'val-generic-password',
+  templateUrl: './generic-password.component.html',
+  styleUrls: ['../base-styles.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class GenericPasswordComponent extends BaseGenericFieldComponent {
+
+  public type: 'password' | 'text' = 'password';
+
+  @Input()
+  public label: string;
+
+  @Input()
+  public placeholder: string;
+
+  @Input()
+  public autocomplete: string;
+
+  public togglePassword() {
+    if (this.type === 'password') {
+      this.type = 'text';
+    } else {
+      this.type = 'password';
+    }
+  }
+}
