b244f8a685c3f4f44fabe3b68be7fab1b09b067c
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-07-17 14:07:58 +0200

    Removed another circular dep

--- projects/ng-core/src/lib/services/scroll-to-top.service.ts
+++ projects/ng-core/src/lib/services/scroll-to-top.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { RenderService } from '@valcome/ng-core';
 import { filter, skip } from 'rxjs/operators';
+import { RenderService } from '../universal/render.service';
 
 @Injectable({ providedIn: 'root' })
 export class ScrollTopService {
