fb980713c0c7565300fe627cbac8baa388085f97
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-07-23 16:41:09 +0200

    Added "extractHostname"

--- projects/ng-core/src/lib/utils/string-utils.ts
+++ projects/ng-core/src/lib/utils/string-utils.ts
@@ -6,4 +6,19 @@ export class StringUtils {
   public static getUniqueString(): string {
     return Math.random().toString(36).substring(2) + Date.now().toString(36);
   }
+
+  public static extractHostname(url: string): string {
+    let hostname;
+
+    if (url.indexOf("//") > -1) {
+      hostname = url.split('/')[2];
+    } else {
+      hostname = url.split('/')[0];
+    }
+
+    hostname = hostname.split(':')[0];
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+  }
 }
