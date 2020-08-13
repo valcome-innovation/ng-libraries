6e36b823a25ea19f0a8f0c9fe6b1745bc66a4148
Simon Kepplinger <simon.kepplinger@valcome.at>
2020-08-13 12:54:05 +0200

    Added exports

--- projects/ng-core/src/public-api.ts
+++ projects/ng-core/src/public-api.ts
@@ -4,12 +4,14 @@ export * from './lib/utils/image.utils';
 export * from './lib/utils/math.utils';
 export * from './lib/utils/share.utils';
 export * from './lib/utils/sound.utils';
+export * from './lib/utils/replay.utils';
 
 export * from './lib/model/share-data';
 export * from './lib/model/point';
 export * from './lib/model/display-value';
 
 export * from './lib/base/components/base.component';
+export * from './lib/base/components/base-replay.component';
 export * from './lib/base/components/base-behavoir-subject.component';
 export * from './lib/base/components/base-subscription.component';
 
