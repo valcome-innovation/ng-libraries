673d5c02a6876fcf345851362de6c987c4741c3d
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2021-02-22 08:21:42 +0100

    Upgrade npm version

--- projects/ts-core/src/public-api.ts
+++ projects/ts-core/src/public-api.ts
@@ -2,3 +2,4 @@ export * from './lib/utils/array.utils';
 export * from './lib/utils/js.utils';
 export * from './lib/utils/performance.utils';
 export * from './lib/utils/string-utils';
+export * from './lib/builder/url.builder';
