89712def9d93e866cb9906abdbcc1b54f66d0bb7
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-02-24 17:38:32 +0100

    And again

--- projects/ng-elements/src/lib/modals/components/modal-frame/modal-frame.component.ts
+++ projects/ng-elements/src/lib/modals/components/modal-frame/modal-frame.component.ts
@@ -1,13 +1,16 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { BaseDynamicModalComponent } from '../base-dynamic-modal.component';
-import { AnimationEasing, fade } from '@valcome/ng-animations';
+import { AnimationEasing, fade, scale } from 'ng-animations';
 
 @Component({
   selector: 'val-modal-frame',
   templateUrl: './modal-frame.component.html',
   styleUrls: ['./modal-frame.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-  animations: [fade({ inTime: 125, outTime: 0, easing: AnimationEasing.EASE_OUT_QUINT })]
+  animations: [
+    fade({ inTime: 125, outTime: 0, easing: AnimationEasing.EASE_OUT_QUINT }),
+    scale({ inTime: 125, outTime: 0, easing: AnimationEasing.EASE_OUT_QUINT })
+  ],
+  encapsulation: ViewEncapsulation.None
 })
 export class ModalFrameComponent extends BaseDynamicModalComponent {
 }
