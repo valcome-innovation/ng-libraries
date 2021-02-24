8e0a6509d7ab90e2c930125e79a7eee69dfe4e86
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2021-02-24 10:03:38 +0100

    Refactor hockeydata score component to be dump

--- projects/ng-hockeydata-widgets/src/lib/components/hockeydata-game-score/hockeydata-game-score.component.ts
+++ projects/ng-hockeydata-widgets/src/lib/components/hockeydata-game-score/hockeydata-game-score.component.ts
@@ -1,39 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { HockeyDataGameReport, HockeyDataIceHockeyService } from '@valcome/ts-hockeydata-api';
+import { Component, Input } from '@angular/core';
+import { HockeyDataGameReport } from '@valcome/ts-hockeydata-api';
 
 @Component({
   selector: 'hockeydata-game-score',
   templateUrl: './hockeydata-game-score.component.html',
   styleUrls: ['./hockeydata-game-score.component.scss']
 })
-export class HockeyDataGameScoreComponent implements OnInit {
-
-  public gameReport?: HockeyDataGameReport;
-  public error = false;
+export class HockeyDataGameScoreComponent {
 
   @Input()
-  public gameId?: string;
+  public gameReport?: HockeyDataGameReport;
 
   @Input()
   public homeLogo?: string;
 
   @Input()
   public awayLogo?: string;
-
-  public constructor(private hockeyDataIceHockeyService: HockeyDataIceHockeyService) {
-  }
-
-  public async ngOnInit(): Promise<void> {
-    this.homeLogo = 'https://api.hockeydata.net/img/icehockey/ebel/team-logos/3328.png';
-    this.awayLogo = 'https://api.hockeydata.net/img/icehockey/ebel/team-logos/7607.png';
-
-    if (this.gameId) {
-      try {
-        this.gameReport = await this.hockeyDataIceHockeyService.getGameReport(this.gameId);
-      } catch (e) {
-        this.error = true;
-      }
-    }
-  }
-
 }
