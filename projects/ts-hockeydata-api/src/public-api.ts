f29a2b601065a585ef348e8d311a814e33fa0e81
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2021-02-24 08:50:53 +0100

    Modify package .json

--- projects/ts-hockeydata-api/src/public-api.ts
+++ projects/ts-hockeydata-api/src/public-api.ts
@@ -2,3 +2,13 @@ export * from './lib/hockeydata-api.module';
 
 export * from './lib/services/icehockey/hockeydata-icehockey.service';
 export * from './lib/services/icehockey/hockeydata-icehockey.dao';
+
+export * from './lib/model/hockeydata-game-score';
+export * from './lib/model/hockeydata-gamedata-schedule';
+export * from './lib/model/hockeydata-gamedata';
+export * from './lib/model/hockeydata-gamereport';
+export * from './lib/model/hockeydata-general-gamedata';
+export * from './lib/model/hockeydata-live-gamedata';
+export * from './lib/model/hockeydata-location';
+export * from './lib/model/hockeydata-period-gamedata';
+export * from './lib/model/hockeydata-period-stats';
