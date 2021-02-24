2985467b769a60b9f9d5cf6ee6f5c4012f2f4abf
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-02-24 17:48:37 +0100

    Modal animations are now a little bit more prominent

--- projects/ng-elements/src/lib/modals/components/modal-frame/modal-frame.component.ts
+++ projects/ng-elements/src/lib/modals/components/modal-frame/modal-frame.component.ts
@@ -5,22 +5,22 @@ import { animate, style, transition, trigger } from '@angular/animations';
 const fade = trigger('fade', [
   transition(':enter', [
     style({ opacity: 0 } as any),
-    animate(`125ms 0ms cubic-bezier(0.22, 1, 0.36, 1)`, style({ opacity: 1 } as any))
+    animate(`200ms 0ms cubic-bezier(0.22, 1, 0.36, 1)`, style({ opacity: 1 } as any))
   ]),
   transition(':leave', [
     style({ opacity: 1 } as any),
-    animate(`125ms 0ms cubic-bezier(0.22, 1, 0.36, 1)`, style({ opacity: 0 } as any))
+    animate(`200ms 0ms cubic-bezier(0.22, 1, 0.36, 1)`, style({ opacity: 0 } as any))
   ])
 ]);
 
 const scale = trigger('scale', [
   transition(':enter', [
     style({ transform: 'scale(0)' } as any),
-    animate(`125ms 0ms cubic-bezier(0.22, 1, 0.36, 1)`, style({ transform: 'scale(1)' } as any))
+    animate(`200ms 0ms cubic-bezier(0.22, 1, 0.36, 1)`, style({ transform: 'scale(1)' } as any))
   ]),
   transition(':leave', [
     style({ transform: 'scale(1)' } as any),
-    animate(`125ms 0ms cubic-bezier(0.22, 1, 0.36, 1)`, style({ transform: 'scale(0)' } as any))
+    animate(`200ms 0ms cubic-bezier(0.22, 1, 0.36, 1)`, style({ transform: 'scale(0)' } as any))
   ])
 ]);
 
