4129f172a2e95728df1dae4febb672e7c5170053
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-03-08 13:06:03 +0100

    Fixed export

--- projects/ng-hockeydata-widgets/src/public-api.ts
+++ projects/ng-hockeydata-widgets/src/public-api.ts
@@ -3,3 +3,5 @@ export * from './lib/hockeydata-widgets.module';
 export * from './lib/components/base-game-score.component';
 export * from './lib/components/hockeydata-game-score/hockeydata-game-score.component';
 export * from './lib/components/hockeydata-game-score-preview/hockeydata-game-score-preview.component';
+export *
+  from './lib/components/hockeydata-standings/hockeydata-standings-compact/hockeydata-standings-compact.component';
