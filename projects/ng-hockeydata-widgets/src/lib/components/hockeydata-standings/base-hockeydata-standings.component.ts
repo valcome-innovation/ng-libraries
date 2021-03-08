64b08933a2713e8d3f8522a82fd9a6b4b8c6c828
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-03-08 16:37:41 +0100

    Added LogoMap type

--- projects/ng-hockeydata-widgets/src/lib/components/hockeydata-standings/base-hockeydata-standings.component.ts
+++ projects/ng-hockeydata-widgets/src/lib/components/hockeydata-standings/base-hockeydata-standings.component.ts
@@ -1,13 +1,16 @@
 import { Directive, Input } from '@angular/core';
 import { HockeyDataTeamStanding } from '@valcome/ts-hockeydata-api';
 
+export type LogoMap = Record<string, string>;
+
 @Directive()
 export abstract class BaseHockeydataStandingsComponent {
+  
   @Input()
   public standings?: HockeyDataTeamStanding[];
 
   @Input()
-  public logos: Record<string, string> = {};
+  public logoMap: LogoMap = {};
 
   @Input()
   public highlighted: string[] = [];
