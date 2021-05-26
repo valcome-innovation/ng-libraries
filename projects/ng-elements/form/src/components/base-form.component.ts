be03184ab39cc07eae5e7a200c4e7c850837dad4
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-05-26 14:18:56 +0200

    Added decorators to base form component

--- projects/ng-elements/form/src/components/base-form.component.ts
+++ projects/ng-elements/form/src/components/base-form.component.ts
@@ -2,11 +2,18 @@ import { BaseBehaviorComponent } from '@valcome/ng-core';
 import { FormGroup } from '@angular/forms';
 import { FormErrorType } from '../model/form-error-type';
 import { InputAutofill } from '../model/input-autofill';
+import { Directive, Input } from '@angular/core';
 
+@Directive()
 export class BaseFormComponent extends BaseBehaviorComponent {
 
+  @Input()
   public form!: FormGroup;
+
+  @Input()
   public isLoading = false;
+
+  @Input()
   public isSubmitted: boolean = false;
 
   public FormErrorType = FormErrorType;
