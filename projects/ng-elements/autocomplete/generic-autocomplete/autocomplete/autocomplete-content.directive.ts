3c339dcd920b18503820335009c94ff6f530d933
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-05-26 12:22:04 +0200

    Upgrade npm version

--- projects/ng-elements/autocomplete/generic-autocomplete/autocomplete/autocomplete-content.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, TemplateRef } from '@angular/core';
-
-@Directive({
-  selector: '[valAutocompleteContent]'
-})
-export class AutocompleteContentDirective {
-  public constructor(public tpl: TemplateRef<any>) {
-  }
-}
