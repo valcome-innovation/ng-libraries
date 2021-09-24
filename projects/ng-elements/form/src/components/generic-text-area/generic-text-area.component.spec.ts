236835ec13e38ed0397edd40eea94da69c91cf90
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-09-24 09:23:41 +0200

    Fixed GenericTextArea test suite

--- projects/ng-elements/form/src/components/generic-text-area/generic-text-area.component.spec.ts
+++ projects/ng-elements/form/src/components/generic-text-area/generic-text-area.component.spec.ts
@@ -1,20 +1,29 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { GenericTextAreaComponent } from './generic-text-area.component';
+import { GenericFormsModule } from '../../generic-forms.module';
+import { FormBuilder } from '@angular/forms';
 
 describe('GenericTextAreaComponent', () => {
 
   let fixture: ComponentFixture<GenericTextAreaComponent>;
   let component: GenericTextAreaComponent;
 
+  let fb: FormBuilder;
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        GenericTextAreaComponent
+      imports: [
+        GenericFormsModule
       ]
     }).compileComponents();
 
+    fb = TestBed.inject(FormBuilder);
+
     fixture = TestBed.createComponent(GenericTextAreaComponent);
     component = fixture.componentInstance;
+
+    component.form = fb.group({ area: fb.control('')});
+    component.formName = 'area';
 
     fixture.detectChanges();
   }));
