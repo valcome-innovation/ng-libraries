5c7f212d265451dd1a456a0f160fe3ad0631f50f
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-10-20 14:20:18 +0200

    Upgrade npm version

--- projects/ts-hockeydata-api/src/lib/hockeydata-api.module.ts
+++ projects/ts-hockeydata-api/src/lib/hockeydata-api.module.ts
@@ -1,18 +1,20 @@
-import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { HockeyDataApiConfig } from './model/types';
 import { HockeyDataIceHockeyDAO } from './services/icehockey/hockeydata-icehockey.dao';
 import { HockeyDataIceHockeyServiceImpl } from './services/icehockey/hockey-data-ice-hockey-impl.service';
 import { HockeyDataIceHockeyService } from './services/icehockey/contracts/hockey-data-ice-hockey.service';
 import { HockeyDataIceHockeySubstitute } from './services/icehockey/contracts/hockey-data-ice-hockey.substitute';
-import { config } from 'rxjs';
 
 export const ICEHOCKEY_API_CONFIG = new InjectionToken<HockeyDataApiConfig>('icehockeyApiKey');
 export const FOOTBALL_API_CONFIG = new InjectionToken<HockeyDataApiConfig>('footballApiKey');
 
 export const iceHockeyServiceFactory = (config?: HockeyDataApiConfig, ...deps: [any, any, any, any, any]): HockeyDataIceHockeyService => {
+  console.log(config);
   if (config) {
+    console.log('A');
     return new HockeyDataIceHockeyServiceImpl(...deps);
   } else {
+    console.log('B');
     return new HockeyDataIceHockeySubstitute();
   }
 }
