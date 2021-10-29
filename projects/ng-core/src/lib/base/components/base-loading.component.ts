b6b9df4f55df6525da2b38b58d20f854ee1f2f3b
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-10-29 09:58:02 +0200

    [ng-core] Added optional change detector to BaseLoadingComponent

--- projects/ng-core/src/lib/base/components/base-loading.component.ts
+++ projects/ng-core/src/lib/base/components/base-loading.component.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { ChangeDetectorRef, Directive } from '@angular/core';
 import { BaseSubscriptionComponent } from './base-subscription.component';
 
 @Directive()
@@ -6,12 +6,19 @@ export class BaseLoadingComponent extends BaseSubscriptionComponent {
 
   public isLoading = false;
 
+  public constructor(protected changeDetector?: ChangeDetectorRef) {
+    super();
+  }
+
   public async load<T>(task: () => Promise<T>): Promise<T> {
     try {
       this.isLoading = true;
+      this.changeDetector?.detectChanges();
+
       return await task();
     } finally {
       this.isLoading = false;
+      this.changeDetector?.detectChanges();
     }
   }
 }
