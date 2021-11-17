d7b8d374cf957bca0c883f5d49899137d4fc2d4f
Simon Kepplinger <simon.kepplinger@valcome.at>
2021-11-17 11:04:05 +0100

    Improved and extended UrlBuilder

--- projects/ts-core/src/lib/builder/url.builder.spec.ts
+++ projects/ts-core/src/lib/builder/url.builder.spec.ts
@@ -12,19 +12,42 @@ describe('UrlBuilder', () => {
     expect(url).toEqual('https://domain.com');
   });
 
-  it('should build full url', () => {
-    builder = new UrlBuilder('https://domain.com');
-    builder.addPath('yeah');
-    builder.addPath(1);
-    builder.addQueryParam('test value', false);
-    builder.addQueryParam('nicer', 'dicer');
+  it('should build url', () => {
+    builder = new UrlBuilder('https://domain.com')
+      .addPath('yeah')
+      .addPath(1)
+      .addQueryParam('test value', false)
+      .addQueryParam('nicer=dicer');
 
     const url = builder.getUrl();
     const path = builder.getPath();
-    const query = builder.getQueryParams();
+    const query = builder.getSearch();
 
     expect(url).toEqual('https://domain.com/yeah/1?test%20value=false&nicer=dicer');
     expect(path).toEqual('yeah/1');
     expect(query).toEqual('test%20value=false&nicer=dicer');
+  });
+
+  it('should add relative path', () => {
+    let relativePaths = [
+      '/long path/mid/file.jpg?v=1&test nicer=dicer',
+      'long path/mid/file.jpg?v=1&test nicer=dicer'
+    ];
+
+    for (const relativePath of relativePaths) {
+      const actual = new UrlBuilder('https://domain.com')
+        .addFullPath(relativePath)
+        .getUrl();
+
+      expect(actual).toEqual('https://domain.com/long%20path/mid/file.jpg?v=1&test%20nicer=dicer');
+    }
+  });
+
+  it('should add path trimmed', () => {
+    const actual = new UrlBuilder('https://domain.com')
+      .addPathTrimmed('/yeah')
+      .getUrl();
+
+    expect(actual).toEqual('https://domain.com/yeah');
   });
 });
--- projects/ts-core/src/lib/builder/url.builder.ts
+++ projects/ts-core/src/lib/builder/url.builder.ts
@@ -1,3 +1,5 @@
+import { StringUtils } from '../utils/string-utils';
+
 export type UriComponent = string | number | boolean;
 
 export class UrlBuilder {
@@ -10,12 +12,79 @@ export class UrlBuilder {
     this.baseUrl = baseUrl;
   }
 
-  public addPath(path: UriComponent): void {
-    this.pathParams.push(encodeURIComponent(path));
+  /**
+   * A relative path is everything after the domain + port part of an url.
+   * E.g./path/name/file.jpg?v=1
+   */
+  public addFullPath(relativePath: string): this {
+    relativePath = StringUtils.trimSlashes(relativePath);
+    const [paths, ...search] = relativePath.split('?');
+
+    this.addPaths(paths.split('/'));
+
+    if (search.length >= 1) {
+      this.addQueryParams(search[0].split('&'));
+    }
+
+    return this;
   }
 
-  public addQueryParam(key: string, value: UriComponent): void {
-    this.queryParams.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+  /**
+   * Adds literal path (including slashes)
+   */
+  public addPath(path: UriComponent): this {
+    this.pathParams.push(encodeURIComponent(path));
+
+    return this;
+  }
+
+  public addPaths(path: UriComponent[]): this {
+    path.forEach(p => this.addPath(p));
+
+    return this;
+  }
+
+  /**
+   * Removes all preceding and trailing slashes in path
+   */
+  public addPathTrimmed(path: UriComponent): this {
+    if (typeof path === 'string') {
+      path = StringUtils.trimSlashes(path);
+    }
+
+    this.addPath(path);
+
+    return this;
+  }
+
+  public addQueryParam(key: string): this
+  public addQueryParam(key: string, value: UriComponent): this
+  public addQueryParam(key: string, value?: UriComponent): this {
+    if (value != undefined) {
+      this.queryParams.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+    } else if (key.includes('=')) {
+      this.queryParams.push(key.split('=').map(k => encodeURIComponent(k)).join('='));
+    }
+
+    return this;
+  }
+
+  public addQueryParams(fullParams: string[]): this {
+    fullParams.forEach(p => this.addQueryParam(p));
+
+    return this;
+  }
+
+  public getPath(): string {
+    return this.pathParams.join('/');
+  }
+
+  public getSearch(): string {
+    return this.queryParams.join('&');
+  }
+
+  public getFullPath(): string {
+    return `${this.getPath()}?${this.getSearch()}`
   }
 
   public getUrl(): string {
@@ -24,17 +93,9 @@ export class UrlBuilder {
     }
 
     if (this.queryParams.length > 0) {
-      this.baseUrl += `?${this.getQueryParams()}`;
+      this.baseUrl += `?${this.getSearch()}`;
     }
 
     return this.baseUrl;
-  }
-
-  public getPath(): string {
-    return this.pathParams.join('/');
-  }
-
-  public getQueryParams(): string {
-    return this.queryParams.join('&');
   }
 }
