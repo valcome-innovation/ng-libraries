f729158fd581467186e9e54b9db0114564617f19
Simon Kepplinger <simon.kepplinger@valcome.at>
2022-06-23 15:47:21 +0200

    [NWS-1117] Remove module import check

--- projects/ng-social-login/src/social-login.module.ts
+++ projects/ng-social-login/src/social-login.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialAuthFacade, SocialAuthServiceConfig } from './social-auth.facade';
 import { GoogleSignInButtonDirective } from './directives/google-sign-in-button.directive';
@@ -29,11 +29,5 @@ export class SocialLoginModule {
         }
       ]
     };
-  }
-
-  public constructor(@Optional() @SkipSelf() parentModule: SocialLoginModule) {
-    if (parentModule) {
-      throw new Error('SocialLoginModule is already loaded. Import it in the AppModule only');
-    }
   }
 }
