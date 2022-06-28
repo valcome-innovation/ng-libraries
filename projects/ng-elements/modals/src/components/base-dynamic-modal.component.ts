257055348910eafa3c7cda06f2c9e401cd66b45c
Simon Kepplinger <simon.kepplinger@valcome.at>
2022-06-28 14:58:27 +0200

    [NWS-1154] Imrpove BaseDynamicModal typing

--- projects/ng-elements/modals/src/components/base-dynamic-modal.component.ts
+++ projects/ng-elements/modals/src/components/base-dynamic-modal.component.ts
@@ -2,19 +2,19 @@ import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 import { BaseBehaviorComponent } from '@valcome/ng-core';
 
 @Directive()
-export abstract class BaseDynamicModalComponent<T = any> extends BaseBehaviorComponent {
+export abstract class BaseDynamicModalComponent<T = any, K = T> extends BaseBehaviorComponent {
 
   public config?: T;
 
   @Output()
-  public modalClose = new EventEmitter<T>();
+  public modalClose = new EventEmitter<K>();
 
   @HostListener('document:keydown.escape')
   public onESCKeydown(): void {
     this.closeModal();
   }
 
-  public closeModal(value?: T): void {
+  public closeModal(value?: K): void {
     this.modalClose.emit(value);
   }
 }
