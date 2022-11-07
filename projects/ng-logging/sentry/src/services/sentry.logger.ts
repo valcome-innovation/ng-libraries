cad16aaa2907d013bb2e78a5658bf9f704aea0ca
Simon Kepplinger <simon.kepplinger@valcome.at>
2022-11-07 13:55:07 +0100

    Fix sentry extra logging data

--- projects/ng-logging/sentry/src/services/sentry.logger.ts
+++ projects/ng-logging/sentry/src/services/sentry.logger.ts
@@ -6,22 +6,22 @@ import { ExtraLoggingData, Logger } from '@valcome/ng-logging';
 export class SentryLogger implements Logger {
 
   public info(message: string, extra?: ExtraLoggingData): void {
-    Sentry.captureMessage(message, { level: 'info', contexts: extra });
+    Sentry.captureMessage(message, { level: 'info', contexts: { extraContext: extra } });
   }
 
   public log(message: string, extra?: ExtraLoggingData): void {
-    Sentry.captureMessage(message, { level: 'log', contexts: extra });
+    Sentry.captureMessage(message, { level: 'log', contexts: { extraContext: extra } });
   }
 
   public warn(message: string, extra?: ExtraLoggingData): void {
-    Sentry.captureMessage(message, { level: 'warning', contexts: extra });
+    Sentry.captureMessage(message, { level: 'warning', contexts: { extraContext: extra } });
   }
 
   public error(message: string, extra?: ExtraLoggingData): void {
-    Sentry.captureMessage(message, { level: 'error', contexts: extra });
+    Sentry.captureMessage(message, { level: 'error', contexts: { extraContext: extra } });
   }
 
   public exception(exception: any, extra?: ExtraLoggingData): void {
-    Sentry.captureException(exception, { level: 'error', contexts: extra });
+    Sentry.captureException(exception, { level: 'error', contexts: { extraContext: extra } });
   }
 }
