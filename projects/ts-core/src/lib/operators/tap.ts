a3870796af7dbba1e529e12c847483aa4843168d
Simon Kepplinger <simon.kepplinger@valcome.at>
2023-03-22 15:41:03 +0100

    Extend Tap's funcionality

--- projects/ts-core/src/lib/operators/tap.ts
+++ projects/ts-core/src/lib/operators/tap.ts
@@ -1,10 +1,28 @@
-export class Tap<T extends any[]> {
+export class Tap<T extends any> {
 
-  public constructor(private callback: (...args: T) => any) {
+  public constructor(private callback: (...args: any[]) => any) {
   }
 
-  public pass(...args: T): T {
+  public pass(arg: T): T {
+    this.callback(arg);
+
+    return arg;
+  }
+
+  public passArray(...args: T[]): T[] {
     this.callback(...args);
+
+    return args;
+  }
+
+  public async passAsync(arg: T): Promise<T> {
+    await this.callback(arg);
+
+    return arg;
+  }
+
+  public async passAsyncArray(...args: T[]): Promise<T[]> {
+    await this.callback(...args);
 
     return args;
   }
