f8019343661651eef393088875980375d3a46b79
Simon Kepplinger <simon.kepplinger@valcome.at>
2024-04-26 10:03:51 +0200

    [EJS-129] Bump @sentry

--- projects/ng-logging/sentry/src/services/sentry.logger.ts
+++ projects/ng-logging/sentry/src/services/sentry.logger.ts
@@ -1,28 +1,43 @@
-import * as Sentry from '@sentry/angular-ivy';
 import { Injectable } from '@angular/core';
 import { ExtraLoggingData, Logger } from '@valcome/ng-logging';
+import { captureException, captureMessage } from '@sentry/angular-ivy';
 
 @Injectable()
 export class SentryLogger
   implements Logger {
 
   public info(message: string, extra?: ExtraLoggingData): void {
-    Sentry.captureMessage(message, { level: 'info', contexts: { additionalInfo: extra } });
+    captureMessage(
+      message,
+      { level: 'info', contexts: { additionalInfo: extra } }
+    );
   }
 
   public log(message: string, extra?: ExtraLoggingData): void {
-    Sentry.captureMessage(message, { level: 'log', contexts: { additionalInfo: extra } });
+    captureMessage(
+      message,
+      { level: 'log', contexts: { additionalInfo: extra } }
+    );
   }
 
   public warn(message: string, extra?: ExtraLoggingData): void {
-    Sentry.captureMessage(message, { level: 'warning', contexts: { additionalInfo: extra } });
+    captureMessage(
+      message,
+      { level: 'warning', contexts: { additionalInfo: extra } }
+    );
   }
 
   public error(message: string, extra?: ExtraLoggingData): void {
-    Sentry.captureMessage(message, { level: 'error', contexts: { additionalInfo: extra } });
+    captureMessage(
+      message,
+      { level: 'error', contexts: { additionalInfo: extra } }
+    );
   }
 
   public exception(exception: any, extra?: ExtraLoggingData): void {
-    Sentry.captureException(exception, { level: 'error', contexts: { additionalInfo: extra } });
+    captureException(
+      exception,
+      { level: 'error', contexts: { additionalInfo: extra } }
+    );
   }
 }
