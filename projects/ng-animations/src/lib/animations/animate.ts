e67644ffdbf581df843f79018e321cfc468245ee
Simon Kepplinger <simon.kepplinger@valcome.at>
2024-07-29 17:27:57 +0200

    Add `baseAnimation` to library

---
+++ projects/ng-animations/src/lib/animations/animate.ts
@@ -0,0 +1,33 @@
+import { AnimationValueConfig, animationValueConfigDefault } from '../model/animation-value-config';
+import { animate, AnimationTriggerMetadata, style, transition, trigger } from '@angular/animations';
+
+export function baseAnimate(animationConfig: AnimationValueConfig<Parameters<typeof style>[0]>,
+                            key: string = 'animate'): AnimationTriggerMetadata {
+  const defaultTransform: AnimationValueConfig<Parameters<typeof style>[0]> = {
+    startValue: { opacity: 0 },
+    endValue: { opacity: 1 }
+  };
+
+  animationConfig = {
+    ...animationValueConfigDefault,
+    ...animationConfig,
+    ...defaultTransform
+  } as AnimationValueConfig<Parameters<typeof style>[0]> ;
+
+  return trigger(key, [
+    transition(':enter', [
+      style(animationConfig.startValue as any),
+      animate(
+        `${animationConfig.inTime}ms ${animationConfig.inDelay}ms ${animationConfig.easing}`,
+        style(animationConfig.endValue as any)
+      )
+    ]),
+    transition(':leave', [
+      style(animationConfig.endValue as any),
+      animate(
+        `${animationConfig.outTime}ms ${animationConfig.outDelay}ms ${animationConfig.easing}`,
+        style(animationConfig.startValue as any)
+      )
+    ])
+  ]);
+}
--- projects/ng-animations/src/public-api.ts
+++ projects/ng-animations/src/public-api.ts
@@ -3,6 +3,7 @@ export * from './lib/animations/scale.animation';
 export * from './lib/animations/bubble-in.animation';
 export * from './lib/animations/scaleY.animation';
 export * from './lib/animations/transform.animation';
+export * from './lib/animations/animate';
 
 export * from './lib/model/animation-config';
 export * from './lib/model/animation-value-config';
